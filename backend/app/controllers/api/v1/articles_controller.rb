dba57152e166c597755e2dcab49763dd5648403b
Gabriel Yuji Yasuda Cardoso <gabrielyyasuda@gmail.com>
2023-04-19 10:00:58 -0300

    Routes configuration

--- backend/app/controllers/api/v1/articles_controller.rb
+++ backend/app/controllers/api/v1/articles_controller.rb
@@ -1,7 +1,16 @@
 class Api::V1::ArticlesController < ApplicationController
   def index
-    #d
     articles = Article.all()
     render json:articles, status: 200
+  end
+
+  def show
+    article = Article.find_by(id:params[:id])
+
+    if article
+      render json:article , status: 200
+    else
+      render json: { error: "Article not found!"}
+    end
   end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
       root "articles#index"
       get "/articles", to: "articles#index"
+      get "/articles/:id", to: "articles#show"
     end
+  end
 end
